cb5b497a6656eddb531e78d898a60d741bbb2d66
Manabu Matsumoto <mmatsumoto1026@gmail.com>
2021-07-15 19:29:14 +0900

    wrong spell in User.js corrected

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: ture },
+    name: { type: String, required: true },
     email: { type: String, required: ture, unique: true },
     password: { type: String, required: true }
 });
